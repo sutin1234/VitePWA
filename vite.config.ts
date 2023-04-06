c9693f9bd77fa3ce3fc4fcbbb6aea11d9a233908
sutin <injitt>
2023-04-06 16:29:13 +0700

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'https://vite-pbo2zv8z1-sutin1234.vercel.app',
+  base: '/',
   plugins: [vue(),
   VitePWA({
     registerType: 'autoUpdate',
