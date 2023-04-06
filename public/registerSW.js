be826e85a290a6a0cb74fce8fbdb3bbae190f4a2
sutin <injitt>
2023-04-06 16:28:12 +0700

    fix: deploy

--- public/registerSW.js
+++ public/registerSW.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     console.log("serviceWorker loaded");
-    navigator.serviceWorker.register("/sw.js", { type: 'classic', scope: '/'});
+    navigator.serviceWorker.register("/sw.js", { type: 'classic', scope: 'https://vite-pbo2zv8z1-sutin1234.vercel.app'});
   });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
+  base: 'https://vite-pbo2zv8z1-sutin1234.vercel.app',
   plugins: [vue(),
   VitePWA({
     registerType: 'autoUpdate',
